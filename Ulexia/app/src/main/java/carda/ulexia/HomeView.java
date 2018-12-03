274d51c4f96ddc0dc52be4f50a644f747f7b7f0b
Colin Lai <colinlai@berkeley.edu>
2018-12-02 17:03:15 -0800

    unstaged files pushed

--- Ulexia/app/src/main/java/carda/ulexia/HomeView.java
+++ Ulexia/app/src/main/java/carda/ulexia/HomeView.java
@@ -1,26 +1,68 @@
 package carda.ulexia;
 
 
+//import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+//import android.widget.EditText;
+//import android.widget.TextView;
+//import android.support.v7.widget.RecyclerView;
+
 
 import com.tomer.fadingtextview.FadingTextView;
+
+// import file name
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeView extends AppCompatActivity {
 
     private Button button;
+
+    // recycler view
+    public List<Block> blocks;
+    public List<String> names;
+//    private RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_view);
 
+//        rv=(RecyclerView)findViewById(R.id.rv);
+//        LinearLayoutManager llm = new LinearLayoutManager(this);
+//        rv.setLayoutManager(llm);
+//        rv.setHasFixedSize(true);
+
         // initialize fading text view properties
         FadingTextView fadingTextView=(FadingTextView)findViewById(R.id.fade);
         fadingTextView.setTimeout(1, FadingTextView.SECONDS);
+
+
+//        Intent intent = getIntent();
+//        if(intent.getStringArrayListExtra("names") != null) {
+//            //Do first time stuff here
+//            names = getIntent().getStringArrayListExtra("names");
+//        }
+
+
+        // check if names array list is empty, should only make new if starting up, no intent
+//        if (names == null){
+//            names = new ArrayList<String>();
+//            names.add("home");
+//        }
+        names = new ArrayList<String>();
+        names.add("home");
+
+        for (String n: names){
+            blocks.add(new Block(n));
+        }
+        // initialize adapter after creating file objects
+//        initializeAdapter();
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -29,10 +71,20 @@ public class HomeView extends AppCompatActivity {
                 openInsertView();
             }
         });
+
     }
 
     public void openInsertView() {
+
         Intent intent = new Intent(this, InsertView.class);
+//        intent.putStringArrayListExtra("names", (ArrayList<String>) names);
         startActivity(intent);
     }
+
+//    public void initializeAdapter(){
+//        RVAdapter adapter = new RVAdapter(blocks, this);
+//        rv.setAdapter(adapter);
+//    }
+
+
 }
--- Ulexia/app/src/main/java/carda/ulexia/InsertView.java
+++ Ulexia/app/src/main/java/carda/ulexia/InsertView.java
@@ -5,7 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InsertView extends AppCompatActivity {
 
@@ -13,11 +24,25 @@ public class InsertView extends AppCompatActivity {
     private TextView tv;
     public static String user_input = "";
 
+    public static String FILE_NAME = "";
+    EditText mEditText;
+    EditText mEditName;
+
+    public List<String> names;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_view);
+
+        mEditText = findViewById(R.id.edit_text);
+        mEditName = findViewById(R.id.edit_name);
+
+        Intent intent = getIntent();
+        names = getIntent().getStringArrayListExtra("names");
+
         button = (Button) findViewById(R.id.button_start);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,9 +52,74 @@ public class InsertView extends AppCompatActivity {
         });
     }
 
-    public void saveUserInput() {
+    public void saveUserInput(View v) {
         tv = (TextView) findViewById(R.id.edit_text);
+
         user_input = tv.getText().toString();
+        String text = mEditText.getText().toString();
+        String filename = mEditName.getText().toString();
+        FILE_NAME = filename + ".txt";
+        // added: save user input to a file in homeview
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            tv.getText(); //.clear();
+            Toast.makeText(this, "Saved to: " + getFilesDir() + "/" + FILE_NAME, Toast.LENGTH_LONG).show();
+            // save to block object
+//            blocks.add(new Block(filename));
+            // save to names arraylist for intent
+            names.add(filename);
+            // load home view
+            Intent intent = new Intent(this, HomeView.class);
+            intent.putStringArrayListExtra("names", (ArrayList<String>) names);
+            startActivity(intent);
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public void load(View v) {
+        FileInputStream fis = null;
+
+        try {
+            fis = openFileInput(FILE_NAME);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public String getUserInput() {
@@ -38,7 +128,7 @@ public class InsertView extends AppCompatActivity {
 
     public void openCardView() {
         Intent intent = new Intent(this, CardView.class);
-        saveUserInput();
+//        saveUserInput();
         startActivity(intent);
     }
 }
